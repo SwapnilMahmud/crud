61c5643d6ea9c6f9435f16c0be3b82534633c307
SwapnilMahmud <mahmudaiub@gmail.com>
2020-11-28 01:42:44 +0600

    p2
    p2

---
+++ add.php
@@ -0,0 +1,41 @@
+<html>
+  <head>
+      <title>Add information</title>
+      <style>
+.bdy{
+  margin:170px;
+}
+label{
+display:left;
+}
+    </style>
+  </head>
+  <body>
+      <div class="bdy">
+        <form action="data.php" method="post">
+
+            <label>Name:</label><input type="text" name="sname" ><br>
+            <label>Address:</label><input type="text area" name="address"><br>
+            <label>Mobile:</label><input type="text" name="phone"><br>
+
+            <label>Class:</label>
+            <select name="cls">
+              <option value="" selected disabled>select class</option>
+
+              <?php
+              $conn=mysqli_connect("localhost","root","","crud") or die("connection faield");
+              $sql="SELECT * FROM  studentclass ";
+              $result=mysqli_query($conn,$sql) or die("unsucessful query");
+              while($row=mysqli_fetch_assoc($result)){
+              ?>
+              <option value="<?php echo $row['cid'];?>"><?php echo $row['cname'];?></option>
+              <?php } ?>
+
+            </select>
+
+          <input type="submit" name="submit">
+        </form>
+      </div>
+
+  </body>
+</html>
---
+++ data.php
@@ -0,0 +1,12 @@
+<?php
+ $stu_name = $_POST['sname'];
+$stu_address = $_POST['address'];
+$stu_cls = $_POST['cls'];
+$stu_phone = $_POST['phone'];
+
+$conn=mysqli_connect("localhost","root","","crud") or die("connection faield");
+$sql="INSERT INTO student(sname,saddress,sclass,sphone) VALUES( '{$stu_name}','{$stu_address}','{$stu_cls}','{$stu_phone}') ";
+$result=mysqli_query($conn,$sql) or die("unsucessful query");
+
+
+?>
